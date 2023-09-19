697a151ac1977637d7d7518a0d9240bd916aea1c
Artem Moroz <deusestmaleficus@gmail.com>
2023-09-20 04:17:20 +0700

    add code

--- src/item.py
+++ src/item.py
@@ -1,8 +1,11 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate = 1.0
+    pay_rate = 0.8
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
@@ -13,10 +16,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
+
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -24,10 +28,57 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        return self.price * self.quantity
+        total_price = self.price * self.quantity
+        return total_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= Item.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        """
+        Класс метод производит инициализацию экземпляров класса
+        из файлов .csv и добавляет их в аттрибут класса all
+        """
+        Item.all = []
+        path = f'../{path}'
+        with open(path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = Item.string_to_number(row['price'])
+                quantity = Item.string_to_number(row['quantity'])
+                Item(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(string: str):
+        """Статик метод принимающий на вход строку.
+        Если строка состоит из цифр, возвращает число.
+        Иначе None"""
+        if '.' in string:
+            temp = string[:string.find('.')]
+            if temp.isdigit():
+                return int(temp)
+            return None
+        elif string.isdigit():
+            return int(string)
+
+    @property
+    def name(self):
+        """
+        Геттер приватного атрибута __name
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        Сеттер приватного атрибута __name
+        """
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
